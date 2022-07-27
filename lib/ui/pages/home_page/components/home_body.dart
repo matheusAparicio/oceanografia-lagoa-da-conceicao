3fafcdaedb23e8c6174d1e678566b8f8537f4f99
Matheus <matheusilva-m@outlook.com>
2022-07-27 12:08:06 -0300

    single child scroll view added into home

--- lib/ui/pages/home_page/components/home_body.dart
+++ lib/ui/pages/home_page/components/home_body.dart
@@ -19,58 +19,73 @@ class _HomeBodyState extends State<HomeBody> {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .8,
-                child: AutoSizeText(
-                  "Para reportar uma ocorrência, pressione reportar.",
-                  style: GoogleFonts.roboto(
-                    color: AppColors().secondaryTextColor,
-                    decoration: TextDecoration.none,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
+      padding: const EdgeInsets.symmetric(vertical: 50),
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .8,
+                  child: AutoSizeText(
+                    "Para reportar uma ocorrência, pressione reportar.",
+                    style: GoogleFonts.roboto(
+                      color: AppColors().secondaryTextColor,
+                      decoration: TextDecoration.none,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
                   ),
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * .4,
-                width: MediaQuery.of(context).size.width * .9,
-                margin: const EdgeInsets.symmetric(vertical: 25),
-                child: OpenStreetMapSearchAndPick(
-                    center: LatLong(-27.60311000897963, -48.472446939370734),
-                    buttonColor: Colors.blue,
-                    buttonText: 'Set Current Location',
-                    onPicked: (pickedData) {}),
-              ),
-              Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: AppColors().primaryColor,
-                  borderRadius: BorderRadius.circular(35)
+                Container(
+                  height: MediaQuery.of(context).size.height * .4,
+                  width: MediaQuery.of(context).size.width * .9,
+                  margin: const EdgeInsets.symmetric(vertical: 25),
+                  child: OpenStreetMapSearchAndPick(
+                      center: LatLong(-27.60311000897963, -48.472446939370734),
+                      buttonColor: Colors.blue,
+                      buttonText: 'Set Current Location',
+                      onPicked: (pickedData) {}),
                 ),
-                width: MediaQuery.of(context).size.width * .8,
-                height: 50,
-                child: AutoSizeText(
-                  "Reportar",
-                  style: GoogleFonts.roboto(
-                    color: AppColors().primaryTextColor,
-                    decoration: TextDecoration.none,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: AppColors().primaryColor,
+                      borderRadius: BorderRadius.circular(35)),
+                  width: MediaQuery.of(context).size.width * .8,
+                  height: 50,
+                  child: AutoSizeText(
+                    "Reportar",
+                    style: GoogleFonts.roboto(
+                      color: AppColors().primaryTextColor,
+                      decoration: TextDecoration.none,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
                   ),
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
                 ),
+              ],
+            ),
+            const SizedBox(height: 300),
+            AutoSizeText(
+              "Algo no final da página",
+              style: GoogleFonts.roboto(
+                color: AppColors().secondaryTextColor,
+                decoration: TextDecoration.none,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
               ),
-            ],
-          ),
-        ],
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
